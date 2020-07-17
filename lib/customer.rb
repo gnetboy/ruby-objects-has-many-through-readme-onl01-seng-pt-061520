14d640a6aaf41df9130b81a06825e2753bfc9599
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-07-17 05:19:04 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -22,6 +22,10 @@ class Customer
           Meal.all.select {|meal|meal.customer ==self}
        end
        
-       
+       def waiters
+           meals.map do |meal|
+              meal.waiter
+          end
+       end
         
 end

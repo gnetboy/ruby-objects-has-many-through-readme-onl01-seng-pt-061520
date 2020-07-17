565caebdad39e200041ba9e8e981476361f03dc2
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-07-17 05:07:03 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,20 @@
 class Meal
-
+      attr_accessor :waiter, :custormer, :total, :tip
+      
+      @@all =[]
+      
+      def initialize(waiter,customer,total,tip=0)
+        
+          @waiter=waiter
+          @custormer=custormer
+          @total=total
+          @tip=tip
+          @@all << self
+        end
+        
+        def self.all 
+            @@all
+        end
+        
+        
 end
